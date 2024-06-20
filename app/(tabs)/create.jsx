fd6da179f15135e876aed72f70bb7462050f8e38
Stefania-Larisa Barabas <stefaniabarabas@gmail.com>
2024-06-20 16:56:34 +0800

    Added create post functionality

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -8,15 +8,18 @@ import {
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import React, { useState } from "react";
-import * as DocumentPicker from "expo-document-picker";
+import * as ImagePicker from "expo-image-picker";
 
 import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
 import { ResizeMode, Video } from "expo-av";
 import { icons } from "../../constants";
 import { router } from "expo-router";
+import { createPost } from "../../lib/appwrite";
+import { useGlobalContext } from "../../context/GlobalProvider";
 
 const Create = () => {
+  const { user } = useGlobalContext();
   const [form, setForm] = useState({
     title: "",
     video: null,
@@ -27,11 +30,14 @@ const Create = () => {
   const [uploading, setUploading] = useState(false);
 
   const openPicker = async (selectedType) => {
-    const result = await DocumentPicker.getDocumentAsync({
-      type:
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes:
         selectedType === "image"
-          ? ["image/png", "image.jpg"]
-          : ["video/mp4", "video/gif"],
+          ? ImagePicker.MediaTypeOptions.Images
+          : ImagePicker.MediaTypeOptions.Videos,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
     });
     if (!result.canceled) {
       if (selectedType === "image") {
@@ -40,19 +46,16 @@ const Create = () => {
       if (selectedType === "video") {
         setForm({ ...form, video: result.assets[0] });
       }
-    } else {
-      setTimeout(() => {
-        Alert.alert("Document picked", JSON.stringify(result, null, 2));
-      }, 100);
     }
   };
 
-  const submit = () => {
+  const submit = async () => {
     if (!form.prompt || !form.thumbnail || !form.thumbnail || !form.video) {
       Alert.alert("Please fill in all fields");
     } else {
       setUploading(true);
       try {
+        await createPost({ ...form, userId: user.$id });
         Alert.alert("Success", "Post uploaded");
         router.push("/home");
       } catch (error) {
@@ -93,8 +96,6 @@ const Create = () => {
                   source={{ uri: form.video.uri }}
                   className="w-full h-64 rounded-2xl"
                   resizeMode={ResizeMode.COVER}
-                  useNativeControls
-                  isLooping
                 />
               ) : (
                 <View className="w-full h-40 px-4 bg-black-100 rounded-xl justify-center items-center">
@@ -121,7 +122,7 @@ const Create = () => {
                   resizeMode="cover"
                 />
               ) : (
-                <View className="w-full h-16 px-4 bg-black-100 rounded-xl justify-center items-center border border-2 border-black-200 flex-row space-y-2">
+                <View className="w-full h-16 px-4 bg-black-100 rounded-xl justify-center items-center border border-black-200 flex-row space-y-2">
                   <Image
                     source={icons.upload}
                     resizeMode="contain"
